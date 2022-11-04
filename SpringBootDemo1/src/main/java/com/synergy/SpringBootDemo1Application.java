25faa09fe7688ea9122aaf3ad4b5dda293d60915
AnkitArora1982 <32397600+AnkitArora1982@users.noreply.github.com>
2022-11-05 00:08:22 +0530

    Update SpringBootDemo1Application.java

--- SpringBootDemo1/src/main/java/com/synergy/SpringBootDemo1Application.java
+++ SpringBootDemo1/src/main/java/com/synergy/SpringBootDemo1Application.java
@@ -8,6 +8,7 @@ public class SpringBootDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemo1Application.class, args);
+		//making changes Ankit
 	}
 
 }
